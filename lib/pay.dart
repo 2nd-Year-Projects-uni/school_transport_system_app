9cd367b3ed1ffbb41d8a671b40de24a9ce9a427b
tharukshinuwanthika28-glitch <tharukshinuwanthika28@gmail.com>
2026-03-17 14:15:32 +0530

    Connect Billing History Screen to pay.dart

--- lib/pay.dart
+++ lib/pay.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'billing_history_screen.dart';
 
 void main() {
   runApp(MaterialApp(debugShowCheckedModeBanner: false, home: HomeScreen()));

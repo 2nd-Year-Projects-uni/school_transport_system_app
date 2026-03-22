1651ce4a6695cb690e84730ef4132f9b821ff9cf
ChinthanaSathyajith <chinthanasathyajith2@gmail.com>
2026-03-23 01:03:56 +0530

    Sync all files to match appfinale branch exactly (preserving history)

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,12 @@
-import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 
-import 'firebase_options.dart';
-import 'login.dart';
-import 'admin/admin_login.dart';
-import 'admin/superadmin_login.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'services/notification_service.dart';
 import 'welcome_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Firebase.initializeApp();
   await NotificationService.instance.init();
   runApp(const MyApp());
 }

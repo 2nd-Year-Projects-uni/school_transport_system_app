8f21a23aada53f4cad7e282d9fb0d14c22fd1298
ChinthanaSathyajith <chinthanasathyajith2@gmail.com>
2026-03-23 10:54:05 +0530

    notifi quick fix

--- lib/driver_payments_page.dart
+++ lib/driver_payments_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
+
 class DriverPaymentsPage extends StatefulWidget {
   const DriverPaymentsPage({Key? key}) : super(key: key);
 
